3d88cce5728f3da2a50dc39a54d4817638a9645e
Somsubhra <das.somsubhra1@gmail.com>
2017-12-26 10:28:18 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main extends Frame implements ActionListener {
+
+    TextArea ta;
+    Button b;
+    Label l1,l2;
+
+    Main() {
+        setSize(500,430);
+        setLayout(null);
+        setVisible(true);
+        setTitle("Word Counter");
+        ta=new TextArea();
+        ta.setBounds(100, 50, 300, 230);
+        add(ta);
+        b=new Button("Count");
+        b.setBounds(200, 300, 80, 30);
+        add(b);
+        b.addActionListener(this);
+
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String word[]=ta.getText().split(" ");
+        l1=new Label("Words: "+word.length);
+        l1.setBounds(100, 330, 100, 20);
+        add(l1);
+
+        l2=new Label("Characters: "+ta.getText().length());
+        l2.setBounds(315, 330, 100, 20);
+        add(l2);
+
+    }
+
+}
